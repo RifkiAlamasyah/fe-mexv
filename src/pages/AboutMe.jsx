1c4e90791cc10361c9c2e7382a0c0634b6aee49c
rifki alamsyah <m.rifki_si19@nusaputra.ac.id>
2024-12-11 14:51:36 +0700

    Ternyata Navlink memiliki sebuah variable isActive ini mentriger halaman itu sedang aktif

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,11 +1,13 @@
 // src/pages/AboutMe.js
 import React from "react";
 import { useParams } from "react-router-dom";
+import Template from "../components/Template";
 
 const AboutMe = () => {
   const { name } = useParams();
 
   return (
+    <Template>
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
       <h1 className="text-4xl font-bold mb-4 text-green-600">About Me</h1>
       <p className="text-lg">
@@ -13,6 +15,7 @@ const AboutMe = () => {
         <span className="font-semibold text-green-700">{name}</span>
       </p>
     </div>
+    </Template>
   );
 };
 
